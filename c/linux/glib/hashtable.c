db2d6e12ffbff25f94322fca28bbf0ac4b3323c2
ccckmit <ccckmit@gmail.com>
2017-12-06 09:15:50 +0800

    resolve hashtable.c update

--- c/linux/glib/hashtable.c
+++ c/linux/glib/hashtable.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Example from IBM tutorial
 
 #include <glib.h>
@@ -18,3 +19,25 @@ int main(int argc, char *argv[]) {
     g_hash_table_destroy(hash);
     return 0;
 }
+=======
+// Example from IBM tutorial
+
+#include <glib.h>
+// https://github.com/ajdecon/play/blob/master/c/glib/hashtable-example.c
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+    
+    g_hash_table_insert(hash,"Jazzy","Cheelsse");
+    g_hash_table_insert(hash,"Mr Darcy","Treats");
+
+    printf("There are %d keys in the hash table\n",
+        g_hash_table_size(hash));
+
+    printf("Jazzy likes %s\n", (char*) g_hash_table_lookup(hash,"Jazzy"));
+
+    g_hash_table_destroy(hash);
+    return 0;
+}
+>>>>>>> 06c9d8fda266177f3a85d377e4938110dd4536e2
